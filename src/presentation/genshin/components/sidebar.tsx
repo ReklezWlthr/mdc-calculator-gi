36fab1dd068f6a1f16da5ce63d2151fe78be7047
GunNapat <napat.wat@innovasive.co.th>
2024-05-03 02:32:29 +0700

    update: add possible sidebar menu

--- src/presentation/genshin/components/sidebar.tsx
+++ src/presentation/genshin/components/sidebar.tsx
@@ -3,7 +3,7 @@ import { GenshinPage } from '@src/domain/genshin/constant'
 const Pill = ({ name, onClick }: { name: string; onClick: () => void }) => {
   return (
     <div
-      className="px-4 py-3 text-sm font-semibold text-white duration-200 rounded-lg cursor-pointer hover:bg-primary-dark"
+      className="px-4 py-3 text-sm font-normal duration-200 rounded-lg cursor-pointer text-gray hover:bg-primary-dark"
       onClick={onClick}
     >
       {name}
@@ -14,11 +14,14 @@ const Pill = ({ name, onClick }: { name: string; onClick: () => void }) => {
 export const Sidebar = ({ onChange }: { onChange: (page: GenshinPage) => void }) => {
   return (
     <div className="w-1/6 p-2 bg-primary-darker">
-      <p className='p-3 font-bold text-white'>Menu</p>
+      <p className="p-3 font-bold text-white">Calculator</p>
       <Pill name="Team Setup" onClick={() => onChange(GenshinPage.TEAM)} />
       <Pill name="Damage Calculator" onClick={() => onChange(GenshinPage.DMG)} />
       <Pill name="ER Requirement" onClick={() => onChange(GenshinPage.ER)} />
       <Pill name="Import" onClick={() => onChange(GenshinPage.IMPORT)} />
+      <p className="p-3 font-bold text-white">Account</p>
+      <Pill name="My Characters" onClick={() => null} />
+      <Pill name="Artifact Inventory" onClick={() => null} />
     </div>
   )
 }
