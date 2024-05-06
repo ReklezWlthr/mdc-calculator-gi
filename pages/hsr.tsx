1220ac1238a74e979bcf8d140502fc78823c9669
GunNapat <napat.wat@innovasive.co.th>
2024-05-07 00:51:02 +0700

    add: hsr landing page

---
+++ pages/hsr.tsx
@@ -0,0 +1,10 @@
+const HsrHomePage = () => {
+  return (
+    <div className="flex w-full h-[calc(100vh-68px)] items-center justify-center gap-5">
+      <img src="https://cdn.wanderer.moe/honkai-star-rail/emotes/pom-pom-gallery---march-7th-6.png" className="w-40 h-40" />
+      <p className="text-white text-8xl">COMING SOON...</p>
+    </div>
+  )
+}
+
+export default HsrHomePage
